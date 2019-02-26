f97efb929870f3f9bcf3ed852cce9c673e9eae7f
Cameron <cameron.milliken@gmail.com>
2019-02-26 15:57:14 -0700

    Completes Entry Controller and storyboard.

---
+++ JournalCK/Controllers/EntryListTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  EntryListTableViewController.swift
+//  JournalCK
+//
+//  Created by Cameron Milliken on 2/26/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import UIKit
+
+class EntryListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+ 
+
+
+
+    
+    // Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+}
--- JournalCK/Model Controller/EntryController.swift
+++ JournalCK/Model Controller/EntryController.swift
@@ -30,4 +30,80 @@ class EntryController {
         }
     }
     
-}
+    // Create
+    func addEntryWith(title: String, body: String, completion: @escaping (Bool) -> Void){
+        let newEntry = Entry(title: title, body: body)
+        self.save(entry: newEntry) { (success) in
+            if success {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+    // Update
+    func updateEntry(entry: Entry, title: String, body: String, completion: @escaping (Bool) -> Void){
+        // update locally
+        entry.title = title
+        entry.body = body
+        
+        // Updates CloudKit
+        CKContainer.default().privateCloudDatabase.fetch(withRecordID: entry.ckRecordID) { (record, error) in
+            if let error = error {
+                print("\(error.localizedDescription) \(error) in function: \(#function)")
+                completion(false)
+                return
+            }
+            guard let record = record else { completion(false) ; return }
+            record[Constants.TitleKey] = title
+            record[Constants.BodyKey] = body
+            
+            let operation = CKModifyRecordsOperation(recordsToSave: [record], recordIDsToDelete: nil)
+            operation.savePolicy = .changedKeys
+            operation.queuePriority = .high
+            operation.qualityOfService = .userInitiated
+            operation.modifyRecordsCompletionBlock = { (records, recordIDs, error) in
+                completion(true)
+            }
+            CKContainer.default().privateCloudDatabase.add(operation)
+        }
+    }
+    
+    // Delete
+    func remove(entry: Entry, completion: @escaping (Bool) -> Void) {
+        
+        // Deletes the entry on CloudKit
+        CKContainer.default().privateCloudDatabase.delete(withRecordID: entry.ckRecordID) { (_, error) in
+            if let error = error {
+                print("\(error.localizedDescription) \(error) in function: \(#function)")
+                completion(false)
+                return
+            } else {
+                print("Successful removed item")
+                completion(true)
+            }
+        }
+        
+        // Deletes the entry locally
+        guard let index = entries.index(of: entry) else { completion(false) ; return }
+        entries.remove(at: index)
+    }
+    
+    // Fetch Entry
+    func fetchEntries(completion: @escaping (Bool)-> Void) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: Constants.recordKey, predicate: predicate)
+        CKContainer.default().privateCloudDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error{
+                print("\(error.localizedDescription) \(error) in function: \(#function)")
+                completion(false)
+                return
+            }
+            guard let records = records else { completion(false) ; return }
+            let entries = records.compactMap { Entry(ckRecord: $0) }
+            self.entries = entries
+            completion(true)
+        }
+    }
+} // End of Class
